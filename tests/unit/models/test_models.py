a623f934db518d6af2711338ace82d55e148ce13
kn1ghtm0nster <diego.quintanilla@proton.me>
2025-10-13 21:08:34 -0400

    feat: add starting test suite and config files

---
+++ tests/unit/models/test_models.py
@@ -0,0 +1,23 @@
+import pytest
+from faker import Faker
+
+from phonebook.models import Contact, PhoneNumber
+
+pytestmark = pytest.mark.django_db
+
+
+@pytest.fixture(scope="session")
+def faker():
+    return Faker()
+
+
+def test_create_contact():
+    c = Contact.objects.create(full_name="Bruce Schneier")
+    p = PhoneNumber.objects.create(
+        phone_number='(703)111-2121', contact=c, is_primary=True
+    )
+
+    assert Contact.objects.count() == 1
+    assert PhoneNumber.objects.count() == 1
+    assert p.contact == c
+    assert p.is_primary is True
