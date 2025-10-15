d646c5f234c814b7a26092df10c0394679b752b9
Diego <84253872+kn1ghtm0nster@users.noreply.github.com>
2025-10-14 20:22:19 -0400

    Setup Test Suite (#2)
    Co-authored-by: kn1ghtm0nster <diego.quintanilla@proton.me>

---
+++ tests/settings.py
@@ -0,0 +1,17 @@
+from config.settings import *  # noqa
+
+DEBUG = False
+ALLOWED_HOSTS = ["testserver", "localhost", "127.0.0.1"]
+
+# Fast, isolated DB
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
+}
+
+# Quieter logs during tests
+LOGGING["root"]["level"] = "CRITICAL"
+for k in LOGGING.get("loggers", {}):
+    LOGGING["loggers"][k]["level"] = "CRITICAL"
